ff299380e656ae593683f9a141c7801dfec6d6f2
breezedu <breeze.du@gmail.com>
2015-10-29 15:49:31 -0400

    pushed DataCamp_lab4.R

---
+++ DataCamp_lab4.R
@@ -0,0 +1,17 @@
+## DataCamp Lab4
+## Duke Statistic
+
+
+# Part A: North Carolina births
+# In 2004, the state of North Carolina released a large data set 
+# containing information on births recorded in this state. 
+# This data set is useful to researchers studying the relation between habits 
+# and practices of expectant mothers and the birth of their children. 
+# We will work with a random sample of observations from this data set.
+
+# Exploratory analysis
+
+# Load the nc data set into our workspace.
+load(url("http://bit.ly/dasi_nc"))
+head(nc)
+

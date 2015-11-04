66facfc4b920f02f667cc9f694c5c8b3a401f915
breezedu <breeze.du@gmail.com>
2015-11-04 18:42:49 -0500

    Update POISSON_gene.R code

--- SQProject/POISSON_gene.R
+++ SQProject/POISSON_gene.R
@@ -87,6 +87,7 @@ summary(logomim)
 
 #use stan
 library("rstan")
+
 gene_code<-"
 data{   #get the data we have
 int <lower=0> N; #number of obs
@@ -95,6 +96,7 @@ int <lower=1,upper=J> gene[N];
 vector[N] x;  #x
 int <lower=0> y[N] ; #y
 }
+
 parameters{ #specify the parameter we want to know 
 vector[J] a;  #random intercept when gene is the level 
 real <lower=0> sigma_a;  #variance of intercept
@@ -102,6 +104,7 @@ real <lower=0> sigma_epsilon; #variance of dispersion
 real beta;    #common slope;
 vector[N] epsilon_raw;
 }
+
 transformed parameters{ #specify the model we will use 
 vector[N] lambda;
 vector[N] epsilon; #amount of dispersion 
@@ -110,6 +113,7 @@ for (i in 1:N)
 for (i in 1:N) 
      lambda[i] <- beta*x[i]+a[gene[i]]+epsilon[i];#specify the group
 }
+
 model { #give the prior distribution
   
   beta ~ normal(0,10);
@@ -118,6 +122,7 @@ model { #give the prior distribution
   y ~ poisson_log(lambda); #y and y_hat should have same type 
 }
 "
+
 N<-dim(table)[1]
 J<-dim(table1)[1]
 gene<-as.numeric(table$gene)
@@ -133,11 +138,13 @@ print(fit1, "a")
 print (fit1, "beta")
 answer1<-extract(fit1, permuted = TRUE)
 effect<-answer1$a
-write.table(effect, "C:/Users/shuaiqi/Desktop/duke/Andrew/project/effectstan.txt"
-, sep="\t")
+
+write.table(effect, "C:/Users/shuaiqi/Desktop/duke/Andrew/project/effectstan.txt", sep="\t")
 
 #check convergence 
 pdf("C:/Users/shuaiqi/Desktop/duke/Andrew/traceplot.pdf")
 traceplot(fit1,pars=c("a","beta"))
 dev.off()
 
+
+############
